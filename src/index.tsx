3fd1083d13290f58139af24f7458963693ae1574
LiShuxue <1149926505@qq.com>
2023-05-30 19:42:45 +0800

    删除web-vitals

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-// import reportWebVitals from './reportWebVitals';
 import App from './App';
 
 import 'antd/dist/reset.css';
@@ -12,5 +11,3 @@ root.render(
     <App />
   </React.StrictMode>
 );
-
-// reportWebVitals(console.log);
